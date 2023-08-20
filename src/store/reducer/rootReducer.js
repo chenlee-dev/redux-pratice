e6b473c0eaefaabef80c87b23220f4aa06db6b8e
Chen Lee <chenlee.work888@gmail.com>
2023-08-20 10:35:05 -0400

    Learn redux-react

---
+++ src/store/reducer/rootReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    user: [
+        { id: 1, name: 'Chen'},
+        { id: 2, name: 'Jun'},
+        { id: 3, name: 'Ju'}
+    ]
+}
+
+const rootReducer = ( state = initState, action ) {
+    
+    return state;
+}
+
+export default rootReducer;
